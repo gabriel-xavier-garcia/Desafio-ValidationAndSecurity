d8f9df2ef8510108b99cdf7cd41ebf37f0d514a5
Gabriel Garcia <gabrielproaj@gmail.com>
2025-11-24 19:24:24 -0300

    UserDTO and RoleDTO

---
+++ src/main/java/com/devsuperior/bds04/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package com.devsuperior.bds04.dto;
+
+
+import com.devsuperior.bds04.entities.Role;
+import com.devsuperior.bds04.entities.User;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String password;
+
+    private Set<RoleDTO> roles = new HashSet<>();
+
+    public UserDTO(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        email = entity.getEmail();
+        password = entity.getPassword();
+        entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+    }
+
+    public UserDTO(User entity, Set<Role> roles){
+        this(entity);
+        entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<RoleDTO> getRoles() {
+        return roles;
+    }
+}
